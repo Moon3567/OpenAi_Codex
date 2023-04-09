95c938516282bf56d9caa553b371f3f980e7e8c8
Moon3567 <Mustafaali3568@gmail.com>
2023-04-09 09:24:57 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) =>{
 
     // fetch data from server
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://openai-codex-1fcx.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
