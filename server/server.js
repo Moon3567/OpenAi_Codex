e92ca5915c17274625d72a786f58f96001cf303c
Moon3567 <Mustafaali3568@gmail.com>
2023-04-10 08:17:52 +0100

    gitignore is now working

--- server/server.js
+++ server/server.js
@@ -9,20 +9,19 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: "Hello from impending doom!",
-  });
-});
+    message: 'Hello World!'
+  })
+})
 
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
@@ -30,20 +29,20 @@ app.post("/", async (req, res) => {
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0, 
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+      max_tokens: 3000, 
+      top_p: 1, 
+      frequency_penalty: 0.5, 
+      presence_penalty: 0, 
     });
+
     res.status(200).send({
       bot: response.data.choices[0].text
     });
 
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ error });
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
-
-});
+})
 
 app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
